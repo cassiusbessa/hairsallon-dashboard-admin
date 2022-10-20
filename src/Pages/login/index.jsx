d91412be007efaea1473679848cbbb3326569270
Anderson Rodrigues <omicron_rpl@hotmail.com>
2022-10-20 00:03:45 -0300

    build: :building_construction: Removed pre-created CSS Files and installed SASS Style-Pre-Processor

--- src/Pages/login/index.jsx
+++ src/Pages/login/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LoginCard from '../../Components/LoginCard';
 import './style.scss';
 
 function Login() {
--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes as Routers, Route } from 'react-router-dom';
+import Login from '../Pages/login';
 
 function Routes() {
   return (
     <Router>
       <Routers>
-        <Route path="/" element={window.localStorage.getItem('tokenAccs') === null ? <Login /> : <Dashboard />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Login />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routers>
     </Router>
   );
