247118fcaed2572dc0c244c111ccd8a4429fcfde
Anderson Rodrigues <omicron_rpl@hotmail.com>
2022-10-20 03:55:06 -0300

    style: :art: Added Theme Provides to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import Routes from './Routes/routes';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <Routes />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Routes />
+    </ThemeProvider>
   );
 }
 
