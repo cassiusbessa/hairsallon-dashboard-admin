27850d1825c943f5d3ad6a188d5bf7ebc2801172
Anderson Rodrigues <omicron_rpl@hotmail.com>
2022-10-20 23:32:09 -0300

    feat: :art: Added Borad component

---
+++ src/Pages/Dashboard/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SideBar from '../../Components/Dashboard/Drawer/dashboard.drawer';
+
+function Dashboard() {
+  return (
+    <SideBar />
+  );
+}
+
+export default Dashboard;
--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes as Routers, Route } from 'react-router-dom';
-import Login from '../Pages/login';
+import Login from '../Pages/Login';
+import Dashboard from '../Pages/Dashboard';
 
 function Routes() {
   return (
     <Router>
       <Routers>
         <Route path="/" element={<Login />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="/board" element={<Dashboard />} />
       </Routers>
     </Router>
   );
